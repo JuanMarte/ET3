c57471884a4ef12aad2148841e1dd44148e825b9
Isaías Gil <isaias.gil@alumnos.uvigo.es>
2025-12-21 23:33:00 +0100

    CORRECION: Ajustados campos de Articulo y Ubicacion al enunciado de la ET2

--- ET3_Equipo/CODIGO/app/estructuras/estructura_ubicacion.js
+++ ET3_Equipo/CODIGO/app/estructuras/estructura_ubicacion.js
@@ -1,10 +1,11 @@
 const estructura_ubicacion = {
     entity: 'ubicacion',
     attributes: {
-        ubicacion_id: {
+        id_site: {
             html: {
                 tag: 'input',
-                type: 'number'
+                type: 'text',
+                component_visible_size: 10
             },
             is_pk: true,
             is_autoincrement: true,
@@ -16,50 +17,7 @@ const estructura_ubicacion = {
                 }
             }
         },
-        ubicacion_nombre: {
-            html: {
-                tag: 'input',
-                type: 'text',
-                component_visible_size: 30
-            },
-            rules: {
-                validations: {
-                    ADD: {
-                        min_size: 3,
-                        max_size: 50,
-                        exp_reg: '^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+$'
-                    },
-                    EDIT: {
-                        min_size: 3,
-                        max_size: 50,
-                        exp_reg: '^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+$'
-                    },
-                    SEARCH: {
-                        exp_reg: '^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]*$'
-                    }
-                }
-            }
-        },
-        ubicacion_direccion: {
-            html: {
-                tag: 'textarea',
-                rows: 3,
-                columns: 40
-            },
-            rules: {
-                validations: {
-                    ADD: {
-                        min_size: 10,
-                        max_size: 200
-                    },
-                    EDIT: {
-                        min_size: 10,
-                        max_size: 200
-                    }
-                }
-            }
-        },
-        ubicacion_ciudad: {
+        site_latitud: {
             html: {
                 tag: 'input',
                 type: 'text',
@@ -68,132 +26,281 @@ const estructura_ubicacion = {
             rules: {
                 validations: {
                     ADD: {
-                        min_size: 3,
-                        max_size: 30,
-                        exp_reg: '^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+$'
+                        min_size: 1,
+                        max_size: 20,
+                        exp_reg: '^[-+]?([0-9]*[.])?[0-9]+$'
                     },
                     EDIT: {
-                        min_size: 3,
-                        max_size: 30,
-                        exp_reg: '^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+$'
+                        min_size: 1,
+                        max_size: 20,
+                        exp_reg: '^[-+]?([0-9]*[.])?[0-9]+$'
                     },
                     SEARCH: {
-                        exp_reg: '^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]*$'
+                        exp_reg: '^[-+]?([0-9]*[.])?[0-9]+$'
                     }
                 }
             }
         },
-        ubicacion_codigo_postal: {
+        site_longitud: {
             html: {
                 tag: 'input',
                 type: 'text',
-                component_visible_size: 6
+                component_visible_size: 20
             },
             rules: {
                 validations: {
                     ADD: {
-                        min_size: 5,
-                        max_size: 5,
-                        exp_reg: '^[0-9]{5}$'
+                        min_size: 1,
+                        max_size: 20,
+                        exp_reg: '^[-+]?([0-9]*[.])?[0-9]+$'
                     },
                     EDIT: {
-                        min_size: 5,
-                        max_size: 5,
-                        exp_reg: '^[0-9]{5}$'
+                        min_size: 1,
+                        max_size: 20,
+                        exp_reg: '^[-+]?([0-9]*[.])?[0-9]+$'
                     },
                     SEARCH: {
-                        exp_reg: '^[0-9]*$'
+                        exp_reg: '^[-+]?([0-9]*[.])?[0-9]+$'
                     }
                 }
             }
         },
-        ubicacion_pais: {
-            html: {
-                tag: 'select',
-                options: ['España', 'Portugal', 'Francia', 'Italia', 'Alemania']
-            },
-            default_value: 'España',
-            rules: {
-                validations: {
-                    ADD: {
-                        exp_reg: '^(España|Portugal|Francia|Italia|Alemania)$'
-                    },
-                    EDIT: {
-                        exp_reg: '^(España|Portugal|Francia|Italia|Alemania)$'
-                    }
-                }
-            }
-        },
-        ubicacion_telefono: {
+        site_altitude: {
             html: {
                 tag: 'input',
                 type: 'text',
-                component_visible_size: 12
+                component_visible_size: 10
             },
             rules: {
                 validations: {
                     ADD: {
-                        exp_reg: '^[0-9]{9}$'
+                        min_size: 1,
+                        max_size: 4,
+                        exp_reg: '^-?[0-9]+$'
                     },
                     EDIT: {
-                        exp_reg: '^[0-9]{9}$'
+                        min_size: 1,
+                        max_size: 4,
+                        exp_reg: '^-?[0-9]+$'
                     },
                     SEARCH: {
-                        exp_reg: '^[0-9]*$'
+                        exp_reg: '^-?[0-9]*$'
                     }
                 }
             }
         },
-        ubicacion_email: {
+        site_locality: {
             html: {
                 tag: 'input',
-                type: 'email',
+                type: 'text',
+                component_visible_size: 40
+            },
+            rules: {
+                validations: {
+                    ADD: {
+                        min_size: 3,
+                        max_size: 40,
+                        exp_reg: '^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\\s]+$'
+                    },
+                    EDIT: {
+                        min_size: 3,
+                        max_size: 40,
+                        exp_reg: '^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\\s]+$'
+                    },
+                    SEARCH: {
+                        exp_reg: '^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\\s]*$'
+                    }
+                }
+            }
+        },
+        site_provider_login: {
+            html: {
+                tag: 'input',
+                type: 'text',
                 component_visible_size: 30
             },
             rules: {
                 validations: {
                     ADD: {
-                        max_size: 50,
-                        exp_reg: '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$'
+                        min_size: 3,
+                        max_size: 30,
+                        exp_reg: '^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\\s]+$'
                     },
                     EDIT: {
-                        max_size: 50,
-                        exp_reg: '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$'
+                        min_size: 3,
+                        max_size: 30,
+                        exp_reg: '^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\\s]+$'
                     },
                     SEARCH: {
-                        exp_reg: '^[a-zA-Z0-9._%+-@]*$'
+                        exp_reg: '^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\\s]*$'
                     }
                 }
             }
         },
-        ubicacion_tipo: {
+        site_north_photo: {
             html: {
-                tag: 'radio',
-                options: ['Oficina', 'Almacén', 'Tienda', 'Sede Central']
+                tag: 'input',
+                type: 'text',
+                component_visible_size: 40
             },
+            is_file_ref: true,
+            file_server_path: 'http://193.147.87.202/ET2/filesuploaded/files_site_north_photo/',
             rules: {
                 validations: {
-                    ADD: {
-                        personalized: true
-                    },
-                    EDIT: {
-                        personalized: true
+                    SEARCH: {
+                        exp_reg: '^[a-zA-Z0-9.]*$'
                     }
                 }
             }
         },
-        ubicacion_activo: {
+        nuevo_site_north_photo: {
             html: {
-                tag: 'checkbox'
+                tag: 'input',
+                type: 'file',
+                multiple: false
             },
-            default_value: '1',
             rules: {
                 validations: {
                     ADD: {
-                        exp_reg: '^(0|1)$'
+                        no_file: true,
+                        max_size_file: 2000000,
+                        type_file: ['image/jpeg', 'image/jpg'],
+                        format_name_file: '^[a-zA-Z0-9.]+\\.(jpg|jpeg)$',
+                        min_size: 7,
+                        max_size: 50
                     },
                     EDIT: {
-                        exp_reg: '^(0|1)$'
+                        max_size_file: 2000000,
+                        type_file: ['image/jpeg', 'image/jpg'],
+                        format_name_file: '^[a-zA-Z0-9.]+\\.(jpg|jpeg)$',
+                        min_size: 7,
+                        max_size: 50
+                    }
+                }
+            }
+        },
+        site_south_photo: {
+            html: {
+                tag: 'input',
+                type: 'text',
+                component_visible_size: 40
+            },
+            is_file_ref: true,
+            file_server_path: 'http://193.147.87.202/ET2/filesuploaded/files_site_south_photo/',
+            rules: {
+                validations: {
+                    SEARCH: {
+                        exp_reg: '^[a-zA-Z0-9.]*$'
+                    }
+                }
+            }
+        },
+        nuevo_site_south_photo: {
+            html: {
+                tag: 'input',
+                type: 'file',
+                multiple: false
+            },
+            rules: {
+                validations: {
+                    ADD: {
+                        no_file: true,
+                        max_size_file: 2000000,
+                        type_file: ['image/jpeg', 'image/jpg'],
+                        format_name_file: '^[a-zA-Z0-9.]+\\.(jpg|jpeg)$',
+                        min_size: 7,
+                        max_size: 50
+                    },
+                    EDIT: {
+                        max_size_file: 2000000,
+                        type_file: ['image/jpeg', 'image/jpg'],
+                        format_name_file: '^[a-zA-Z0-9.]+\\.(jpg|jpeg)$',
+                        min_size: 7,
+                        max_size: 50
+                    }
+                }
+            }
+        },
+        site_east_photo: {
+            html: {
+                tag: 'input',
+                type: 'text',
+                component_visible_size: 40
+            },
+            is_file_ref: true,
+            file_server_path: 'http://193.147.87.202/ET2/filesuploaded/files_site_east_photo/',
+            rules: {
+                validations: {
+                    SEARCH: {
+                        exp_reg: '^[a-zA-Z0-9.]*$'
+                    }
+                }
+            }
+        },
+        nuevo_site_east_photo: {
+            html: {
+                tag: 'input',
+                type: 'file',
+                multiple: false
+            },
+            rules: {
+                validations: {
+                    ADD: {
+                        no_file: true,
+                        max_size_file: 2000000,
+                        type_file: ['image/jpeg', 'image/jpg'],
+                        format_name_file: '^[a-zA-Z0-9.]+\\.(jpg|jpeg)$',
+                        min_size: 7,
+                        max_size: 50
+                    },
+                    EDIT: {
+                        max_size_file: 2000000,
+                        type_file: ['image/jpeg', 'image/jpg'],
+                        format_name_file: '^[a-zA-Z0-9.]+\\.(jpg|jpeg)$',
+                        min_size: 7,
+                        max_size: 50
+                    }
+                }
+            }
+        },
+        site_west_photo: {
+            html: {
+                tag: 'input',
+                type: 'text',
+                component_visible_size: 40
+            },
+            is_file_ref: true,
+            file_server_path: 'http://193.147.87.202/ET2/filesuploaded/files_site_west_photo/',
+            rules: {
+                validations: {
+                    SEARCH: {
+                        exp_reg: '^[a-zA-Z0-9.]*$'
+                    }
+                }
+            }
+        },
+        nuevo_site_west_photo: {
+            html: {
+                tag: 'input',
+                type: 'file',
+                multiple: false
+            },
+            rules: {
+                validations: {
+                    ADD: {
+                        no_file: true,
+                        max_size_file: 2000000,
+                        type_file: ['image/jpeg', 'image/jpg'],
+                        format_name_file: '^[a-zA-Z0-9.]+\\.(jpg|jpeg)$',
+                        min_size: 7,
+                        max_size: 50
+                    },
+                    EDIT: {
+                        max_size_file: 2000000,
+                        type_file: ['image/jpeg', 'image/jpg'],
+                        format_name_file: '^[a-zA-Z0-9.]+\\.(jpg|jpeg)$',
+                        min_size: 7,
+                        max_size: 50
                     }
                 }
             }
